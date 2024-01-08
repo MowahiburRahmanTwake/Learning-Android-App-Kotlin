acad0a5abc44056c5ef9fe58022bff374550af87
twake1234 <twake.edu@gmail.com>
2024-01-08 12:18:25 +0600

    Learning for bdskill

---
+++ app/src/main/java/com/bogo/myfirstapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.bogo.myfirstapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var numberOneEt: EditText
+    private lateinit var numberTwoEt: EditText
+    private lateinit var calculateBtn: Button
+    private lateinit var resultTv: TextView
+
+    private var sum = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        numberOneEt =findViewById(R.id.numberOneEt)
+        numberTwoEt =findViewById(R.id.numberTwoEt)
+        calculateBtn =findViewById(R.id.calcutatorBtn)
+        resultTv =findViewById(R.id.resultTV)
+
+        if(savedInstanceState != null){
+            sum = savedInstanceState.getInt("sum")
+            resultTv.text = sum.toString()
+        }
+
+
+        calculateBtn.setOnClickListener {
+            val n1String = numberOneEt.text.toString()
+            val n2String = numberTwoEt.text.toString()
+            val n1 =n1String.toInt()
+            val n2 =n2String.toInt()
+            sum = n1 + n2
+            resultTv.text = sum.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("sum", sum)
+        super.onSaveInstanceState(outState)
+    }
+
+}
